a1116646aaf3da3455940287f9bab00748754083
shu liu <shu@liu.net>
2024-12-09 10:09:19 -0800

    Add a for loop test file

--- for_loop_test.py
+++ for_loop_test.py
@@ -22,7 +22,7 @@ for num in single_list:
 
 # How many 3 in nested_list?
 nested_list = [[5, 1, 2, 4, 1, 2, 5, 5, 2, 2, 3, 4, 1, 3, 5, 5, 5, 3, 4, 5], [5, 3, 2, 4, 5, 1, 3, 2, 1, 2, 3, 2, 2, 1, 1, 1, 5, 5, 1, 1], [1, 5, 3, 1, 3, 5, 1, 3, 2, 1, 4, 5, 3, 3, 1, 5, 1, 2, 2, 5], [2, 5, 2, 5, 1, 2, 1, 3, 3, 5, 1, 4, 5, 3, 1, 1, 5, 3, 3, 3], [3, 1, 3, 2, 4, 5, 2, 1, 4, 1, 5, 1, 4, 3, 1, 4, 4, 5, 1, 3], [5, 4, 4, 5, 3, 2, 4, 2, 3, 5, 5, 2, 4, 5, 5, 1, 3, 1, 3, 2], [1, 1, 5, 2, 4, 2, 3, 1, 1, 4, 5, 2, 2, 5, 1, 5, 3, 4, 4, 2], [5, 2, 1, 4, 1, 4, 2, 2, 1, 4, 5, 5, 1, 1, 3, 3, 5, 3, 5, 2], [2, 5, 2, 5, 4, 4, 4, 1, 3, 5, 2, 1, 3, 5, 3, 2, 1, 5, 2, 1], [2, 1, 2, 1, 1, 3, 5, 2, 3, 4, 3, 5, 5, 4, 4, 4, 2, 2, 3, 2], [2, 5, 5, 4, 1, 2, 5, 1, 5, 5, 5, 3, 1, 4, 1, 4, 1, 4, 2, 2], [3, 2, 4, 2, 4, 5, 5, 4, 2, 2, 1, 1, 1, 4, 4, 4, 5, 2, 1, 1], [2, 5, 3, 5, 1, 1, 5, 3, 4, 4, 1, 5, 1, 4, 5, 4, 4, 2, 4, 1], [2, 3, 4, 2, 1, 1, 5, 2, 1, 5, 3, 3, 5, 3, 2, 3, 1, 1, 2, 2], [3, 1, 3, 3, 1, 5, 5, 4, 1, 5, 1, 1, 2, 2, 1, 1, 2, 4, 4, 1]]
-pprint(nested_list)
+# pprint(nested_list)
 
 counter = []
 for inner in nested_list: # take out every list inside the nested_list
@@ -31,5 +31,41 @@ for inner in nested_list: # take out every list inside the nested_list
 		if number == 3:
 			count = count + 1
 	counter.append(count)
-print(counter)
+# print(counter)
+
+
+# 12/8/2024
+my_nested_list = [
+                    [1,2,3],
+                    [2,2,3],
+                    [3,3,1]
+                                ]
+# pprint(my_nested_list)
+
+# how many 3 in my_nested_list?
+
+counter = []
+for inner in my_nested_list:
+	sum = 0
+	for number in inner:
+		if number == 3:
+			sum = sum + 1
+	counter.append(sum)
+# print(counter)
+
+
+# total amount of 3
+
+sum = 0
+for inner in my_nested_list:
+	for number in inner:
+		if number == 3:
+			sum = sum + 1
+# print(sum)
+
+
+
+# nested_list
+
+pprint(nested_list)
 
